033f0ab712fc46818aa347f4d0114e3e250c09e7
Husnain Ali <129303938+Husnain-allly@users.noreply.github.com>
2025-03-17 21:39:17 +0500

    App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from './Pages/HomePage';
+import ParallelCodePage from "./Pages/ParallelCodePage";
 import UploadFilesPage from './Pages/Upload'; // Import the UploadPage component
 import Login from './Pages/Login'; // Import the Login component
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -10,9 +11,11 @@ function App() {
       <div className="App">
         <Router>
           <Routes>
+           
             <Route path="/" element={<HomePage />} />
             <Route path="/login" element={<Login />} />
             <Route path="/upload" element={<UploadFilesPage />} />
+             <Route path="/parallel-code" element={<ParallelCodePage />} />
           </Routes>
         </Router>
       </div>
